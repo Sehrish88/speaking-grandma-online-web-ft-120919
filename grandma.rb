2e8f1e731559710d606f8138d493c256255f4c5e
Sehrish Balouch <sehrishbalouch4@gmail.com>
2019-12-10 16:01:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,5 +16,7 @@
 def speak_to_grandma (phrase)
   if phrase == "I LOVE YOU GRANDMA"
     puts "I LOVE YOU TOO PUMPKIN"
-  elsif   
+  elsif phrase == phrase.downcase
+    puts "HUH? SPEAK UP SONNY!"
+    
   
